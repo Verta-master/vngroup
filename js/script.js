9f893ed6ac7ddc00cbf9a97cb8e443bf49397309
Verta-master <vertyagina@gmail.com>
2019-10-29 13:36:47 +0600

    Update 2019-10-29T07:36:43.450Z

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ $('.menu__btn').click(function() {
   $('.menu__list').slideToggle();
 })
 
-//if (window.innerWidth < 766) {
-//  
-//}
+if (window.innerWidth < 1022) {
+  $('.nav__link').click(function(e) {
+    e.preventDefault();
+    $(this).next().slideToggle();
+  });
+  $('.footer__block--menu .footer__title').click(function() {
+    $('.footer__menu').slideToggle();
+  });
+  $('.footer__block--address .footer__title').click(function() {
+    $('address').slideToggle();
+  });
+}
